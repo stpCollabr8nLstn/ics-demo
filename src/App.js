f336f782502240c6b30059e511994709e0914c03
amrios <amrios2817@gmail.com>
2018-05-30 16:54:01 -0400

    intial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Sign up for FEDucation</p>
+      </div>
+    );
+  }
+}
+
+export default App;
